c8efa5d54d09d3b7e986730313e90a5d53580dba
hetaljoshi2425 <hetaljoshi2425@gmail.com>
2023-07-12 18:02:38 +0530

    home oage complete,budget page changes

--- src/app/pages/settings-navbar/settings-navbar.component.ts
+++ src/app/pages/settings-navbar/settings-navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+// import { Router, } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-settings-navbar',
   templateUrl: './settings-navbar.component.html',
   styleUrls: ['./settings-navbar.component.css']
 })
 export class SettingsNavbarComponent {
- 
+  activeLink: string;
  constructor(private router:Router){
 
  }
- 
+ ngOnInit() {
+  this.router.events.subscribe(event => {
+    if (event instanceof NavigationEnd) {
+      this.activeLink = event.urlAfterRedirects;
+    }
+  });
+}
+
   
 }
